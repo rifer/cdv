06022d5308e3bd6b891ade0c3c081fb047e160ac
Rodrigo Iglesias <you@example.com>
2015-10-16 13:00:07 +0200

    some home and admin structures and styles

--- web/scripts/cdv.js
+++ web/scripts/cdv.js
@@ -1,1 +1,21 @@
-"use strict";$(document).ready(function(){setTimeout(function(){$(".hr-image.animated").addClass("bounceInDown").removeClass("hide")},500),$(".menu-icon").on("click",function(){$(".menu-wrapper").toggleClass("show-menu")});new Waypoint.Sticky({element:$(".main-nav")[0]})});
+'use strict';
+$(document).ready(function(){
+    setTimeout(function(){
+        $('.hr-image.animated').addClass('bounceInDown').removeClass('hide');          
+    },500);
+
+    $('.menu-icon').on('click', function(){
+    	$('.menu-wrapper').toggleClass('show-menu');
+    })
+
+	var sticky = new Waypoint.Sticky({
+	  element: $('.main-nav')[0]
+	})
+
+});
+$('a[data-next]').on('click',function(event){
+	    event.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $('.menu-wrapper').offset().top
+    }, 1200);
+});
